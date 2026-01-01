564f66bb9a8d8d668f749fb3f98c8d3ad7f9206a
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2026-01-01 17:15:31 +0100

    Bind the components in app.component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { HeroComponent } from "../../components/hero/hero.component";
+import { AboutComponent } from "../../components/about/about.component";
+import { SkillsComponent } from "../../components/skills/skills.component";
+import { ProjectsComponent } from "../../components/projects/projects.component";
+import { ContactComponent } from "../../components/contact/contact.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, HeroComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
