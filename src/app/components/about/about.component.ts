d112fc3accb9609e84e900200238a658cff7816c
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2026-01-02 20:25:44 +0100

    Translation the about section

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslationService } from '../../services/translation.service';
 
 @Component({
   selector: 'app-about',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+
+  constructor(public translation: TranslationService) {}
 
 }
