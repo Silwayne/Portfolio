6b72033c9df6dbd4f45ded3947cf9576a1e0d66f
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2026-01-04 20:28:04 +0100

    Privacy policy added, some fixes

--- src/app/pages/privacy/privacy.component.ts
+++ src/app/pages/privacy/privacy.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { TranslationService } from '../../services/translation.service';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-privacy',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, NavbarComponent, FooterComponent],
   templateUrl: './privacy.component.html',
   styleUrl: './privacy.component.scss'
 })
 export class PrivacyComponent {
+
+  constructor(public translation: TranslationService) {}
 
 }
